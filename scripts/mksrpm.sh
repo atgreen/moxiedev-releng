4bdbbab74500b1d61aee66e722a3a52d03dd32d9
Anthony Green <green@moxielogic.com>
2016-08-30 18:19:16 -0400

    binutils and gdb only

--- scripts/mksrpm.sh
+++ scripts/mksrpm.sh
@@ -10,11 +10,12 @@ BUILDNUM=`cat BUILDNUM`
 BUILDNUM=`expr $BUILDNUM + 1`
 echo $BUILDNUM > BUILDNUM
 DATE=`date +"%a %b %d %Y"`
-GCC_VERSION=`cat gcc/gcc/BASE-VER`
+
+#GCC_VERSION=`cat gcc/gcc/BASE-VER`
 GDB_VERSION=`cat binutils-gdb/gdb/version.in | sed -e "s/\-//"`
 BINUTILS_VERSION=`cat binutils-gdb/bfd/version.m4 | awk -F "[,() ]" -f scripts/v.awk | tr -d []`
-NEWLIB_VERSION=`cat src/newlib/configure | awk -F "\"" -f scripts/nv.awk` 
-QEMU_VERSION=`cat qemu-moxie/VERSION`
+#NEWLIB_VERSION=`cat src/newlib/configure | awk -F "\"" -f scripts/nv.awk` 
+#QEMU_VERSION=`cat qemu-moxie/VERSION`
 
 if ! test -d dist; then
   mkdir dist
@@ -28,37 +29,37 @@ for i in moxie-elf moxie-rtems moxiebox; do
   cp specs/moxielogic-$i-binutils.spec.in dist/moxielogic-$i-binutils.spec
   sed -i "s/@VERSION@/$BINUTILS_VERSION/g" dist/moxielogic-$i-binutils.spec
   sed -i "s/@DATE@/$DATE/g" dist/moxielogic-$i-binutils.spec
-  cp specs/moxielogic-$i-gcc.spec.in dist/moxielogic-$i-gcc.spec
-  sed -i "s/@VERSION@/$GCC_VERSION/g" dist/moxielogic-$i-gcc.spec
-  sed -i "s/@DATE@/$DATE/g" dist/moxielogic-$i-gcc.spec
-  cp specs/moxielogic-$i-newlib.spec.in dist/moxielogic-$i-newlib.spec
-  sed -i "s/@VERSION@/$NEWLIB_VERSION/g" dist/moxielogic-$i-newlib.spec
-  sed -i "s/@DATE@/$DATE/g" dist/moxielogic-$i-newlib.spec
+#  cp specs/moxielogic-$i-gcc.spec.in dist/moxielogic-$i-gcc.spec
+#  sed -i "s/@VERSION@/$GCC_VERSION/g" dist/moxielogic-$i-gcc.spec
+#  sed -i "s/@DATE@/$DATE/g" dist/moxielogic-$i-gcc.spec
+#  cp specs/moxielogic-$i-newlib.spec.in dist/moxielogic-$i-newlib.spec
+#  sed -i "s/@VERSION@/$NEWLIB_VERSION/g" dist/moxielogic-$i-newlib.spec
+#  sed -i "s/@DATE@/$DATE/g" dist/moxielogic-$i-newlib.spec
   cp specs/moxielogic-$i-gdb.spec.in dist/moxielogic-$i-gdb.spec
   sed -i "s/@VERSION@/$GDB_VERSION/g" dist/moxielogic-$i-gdb.spec
   sed -i "s/@DATE@/$DATE/g" dist/moxielogic-$i-gdb.spec
   
   sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-$i-binutils.spec
-  sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-$i-gcc.spec
-  sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-$i-newlib.spec
+#  sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-$i-gcc.spec
+#  sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-$i-newlib.spec
   sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-$i-gdb.spec
 done;
 
-cp specs/moxielogic-rtems4.11.spec.in dist/moxielogic-rtems4.11.spec
-sed -i "s/@VERSION@/4.11/g" dist/moxielogic-rtems4.11.spec
-sed -i "s/@DATE@/$DATE/g" dist/moxielogic-rtems4.11.spec
-sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-rtems4.11.spec
+#cp specs/moxielogic-rtems4.11.spec.in dist/moxielogic-rtems4.11.spec
+#sed -i "s/@VERSION@/4.11/g" dist/moxielogic-rtems4.11.spec
+#sed -i "s/@DATE@/$DATE/g" dist/moxielogic-rtems4.11.spec
+#sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-rtems4.11.spec
 
-cp specs/moxielogic-qemu.spec.in dist/moxielogic-qemu.spec
-sed -i "s/@VERSION@/$QEMU_VERSION/g" dist/moxielogic-qemu.spec
-sed -i "s/@DATE@/$DATE/g" dist/moxielogic-qemu.spec
-sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-qemu.spec
+#cp specs/moxielogic-qemu.spec.in dist/moxielogic-qemu.spec
+#sed -i "s/@VERSION@/$QEMU_VERSION/g" dist/moxielogic-qemu.spec
+#sed -i "s/@DATE@/$DATE/g" dist/moxielogic-qemu.spec
+#sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-qemu.spec
 
 
-(cd qemu; make clean > /dev/null 2>&1)
-tar --exclude .git -czf dist/moxielogic-qemu.tar.gz qemu-moxie
+#(cd qemu; make clean > /dev/null 2>&1)
+#tar --exclude .git -czf dist/moxielogic-qemu.tar.gz qemu-moxie
 
-rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" --define "VERSION $QEMU_VERSION" --define "_sourcedir dist" --define "_srcrpmdir dist" -bs dist/moxielogic-qemu.spec
+#rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" --define "VERSION $QEMU_VERSION" --define "_sourcedir dist" --define "_srcrpmdir dist" -bs dist/moxielogic-qemu.spec
 
 tar \
 --exclude .git \
@@ -69,23 +70,23 @@ tar \
 --exclude binutils-gdb/gprof \
 -czf dist/moxie-gdb.tar.gz binutils-gdb
 
-(cd gcc; ./contrib/gcc_update --touch)
-tar \
---exclude .svn \
---exclude libjava \
---exclude libada \
---exclude gnattools \
---exclude zlib \
---exclude libffi \
---exclude libmudflap \
---exclude fortran \
---exclude boehm-gc \
---exclude libgfortran \
---exclude ada \
---exclude libobjc \
---exclude ppl \
---exclude cloog-ppl \
--czf dist/moxie-gcc.tar.gz gcc
+#(cd gcc; ./contrib/gcc_update --touch)
+#tar \
+# --exclude .svn \
+# --exclude libjava \
+# --exclude libada \
+# --exclude gnattools \
+# --exclude zlib \
+# --exclude libffi \
+# --exclude libmudflap \
+# --exclude fortran \
+# --exclude boehm-gc \
+# --exclude libgfortran \
+# --exclude ada \
+# --exclude libobjc \
+# --exclude ppl \
+# --exclude cloog-ppl \
+# -czf dist/moxie-gcc.tar.gz gcc
 
 tar \
 --exclude .git \
@@ -96,9 +97,9 @@ tar \
 --exclude binutils-gdb/gold \
 -czf dist/moxie-binutils.tar.gz binutils-gdb
 
-tar \
---exclude CVS \
--czf dist/moxie-newlib.tar.gz src
+#tar \
+#--exclude CVS \
+#-czf dist/moxie-newlib.tar.gz src
 
 for i in moxie-elf moxie-rtems moxiebox; do
 
@@ -108,7 +109,7 @@ rpmbuild \
 --define "VERSION $GDB_VERSION" \
 -bs dist/moxielogic-$i-gdb.spec
 
-rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" --define "VERSION $GCC_VERSION" --define "_sourcedir dist" --define "_srcrpmdir dist" -bs dist/moxielogic-$i-gcc.spec
+# rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" --define "VERSION $GCC_VERSION" --define "_sourcedir dist" --define "_srcrpmdir dist" -bs dist/moxielogic-$i-gcc.spec
 
 rpmbuild \
 --define "_sourcedir dist" \
@@ -116,29 +117,29 @@ rpmbuild \
 --define "VERSION $BINUTILS_VERSION" \
 -bs dist/moxielogic-$i-binutils.spec
 
-rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" \
---define "_sourcedir dist" \
---define "_srcrpmdir dist" \
--bs dist/moxielogic-$i-newlib.spec
+#rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" \
+#--define "_sourcedir dist" \
+#--define "_srcrpmdir dist" \
+#-bs dist/moxielogic-$i-newlib.spec
 
 done;
 
-cp dist/moxielogic-moxie-elf-gcc.spec dist/bootstrap-moxie-elf-gcc.spec
-perl -p -i -e 's/moxielogic-moxie-elf-gcc/bootstrap-moxie-elf-gcc/g' dist/bootstrap-moxie-elf-gcc.spec
-perl -p -i -e 's/with_newlib 1/with_newlib 0/g' dist/bootstrap-moxie-elf-gcc.spec
-rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" --define "VERSION $GCC_VERSION" --define "_sourcedir dist" --define "_srcrpmdir dist" -bs dist/bootstrap-moxie-elf-gcc.spec
+#cp dist/moxielogic-moxie-elf-gcc.spec dist/bootstrap-moxie-elf-gcc.spec
+#perl -p -i -e 's/moxielogic-moxie-elf-gcc/bootstrap-moxie-elf-gcc/g' dist/bootstrap-moxie-elf-gcc.spec
+#perl -p -i -e 's/with_newlib 1/with_newlib 0/g' dist/bootstrap-moxie-elf-gcc.spec
+#rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" --define "VERSION $GCC_VERSION" --define "_sourcedir dist" --define "_srcrpmdir dist" -bs dist/bootstrap-moxie-elf-gcc.spec
 
-tar \
---exclude .git \
--czf dist/rtems.tar.gz RTEMS
+#tar \
+#--exclude .git \
+#-czf dist/rtems.tar.gz RTEMS
 
-rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" \
---define "_sourcedir dist" \
---define "_srcrpmdir dist" \
--bs dist/moxielogic-rtems4.11.spec
+#rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" \
+#--define "_sourcedir dist" \
+#--define "_srcrpmdir dist" \
+#-bs dist/moxielogic-rtems4.11.spec
 
 cp scripts/RPM-GPG-KEY-MoxieLogic dist
-for TAG in f20 f21 el6 el7; do
+for TAG in el7; do
     EXPR=s/TAG/$TAG/g
     cp scripts/Moxie_Logic.repo.in dist/Moxie_Logic.repo
     cp specs/moxielogic-repo.spec.in dist/moxielogic-repo-$TAG.spec
