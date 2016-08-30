8344333bc4dcc6a92cf822c804d41f72a022c24e
Anthony Green <green@moxielogic.com>
2016-08-30 18:30:59 -0400

    Build some rpms

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ MAINTAINER green@moxielogic.org
 
 RUN yum -y install gcc gcc-c++ rpmbuild make patch autoconf automake mpfr-devel libgmp-devel mpc-devel flex bison rpm-build
 
-CMD pwd && ls -l /opt && cd /opt && ./scripts/mksrpm.sh
+CMD cd /opt && ./scripts/mksrpm.sh && ./scripts/mkrpms.sh
+
 
 
 
--- scripts/mkrpms.sh
+++ scripts/mkrpms.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -x
 
-PACKAGES="moxielogic-qemu moxielogic-moxie-elf-binutils bootstrap-moxie-elf-gcc moxielogic-moxie-elf-newlib moxielogic-moxie-elf-gcc moxielogic-moxie-elf-gdb"
+# PACKAGES="moxielogic-qemu moxielogic-moxie-elf-binutils bootstrap-moxie-elf-gcc moxielogic-moxie-elf-newlib moxielogic-moxie-elf-gcc moxielogic-moxie-elf-gdb"
+PACKAGES="moxielogic-moxie-elf-binutils moxielogic-moxie-elf-gdb"
 
 for i in moxie-elf moxie-rtems moxiebox; do
-  PACKAGES="$PACKAGES moxielogic-$i-binutils moxielogic-$i-newlib moxielogic-$i-gcc moxielogic-$i-gdb";
+    #  PACKAGES="$PACKAGES moxielogic-$i-binutils moxielogic-$i-newlib moxielogic-$i-gcc moxielogic-$i-gdb";
+  PACKAGES="$PACKAGES moxielogic-$i-binutils moxielogic-$i-gdb";    
 done;
 
 PACKAGES="moxielogic-repo-$TAG $PACKAGES"
