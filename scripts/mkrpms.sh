19b67fe8cb4fb1dc753db64f4929cb2067965d3e
Anthony Green <green@moxielogic.com>
2016-08-31 08:42:09 -0400

    Build newlib and new gcc

--- scripts/mkrpms.sh
+++ scripts/mkrpms.sh
@@ -15,5 +15,12 @@ done
 rpmbuild --rebuild dist/bootstrap-moxie-elf-gcc*src.rpm;
 rpm -hiv /root/rpmbuild/RPMS/x86_64/bootstrap-moxie-elf-gcc*.rpm;
 
+for i in $TARGETS; do
+    rpmbuild --rebuild dist/moxielogic-$i-newlib*src.rpm;
+    rpm -hiv /root/rpmbuild/RPMS/noarch/moxielogic-$i-newlib*.rpm;
+    rpmbuild --rebuild dist/moxielogic-$i-gcc*src.rpm;
+    rpm -hiv /root/rpmbuild/RPMS/x86_64/moxielogic-$i-gcc*.rpm;
+done
+
 mkdir rpms
 find /root/rpmbuild -name \*.rpm | xargs -n 1 -I RPMFILE cp -a RPMFILE rpms
