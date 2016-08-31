72d636d4808313b57ad3c68334b1795b9bfeb670
Anthony Green <green@moxielogic.com>
2016-08-30 21:35:25 -0400

    grab right rpm artifacts

--- scripts/mkrpms.sh
+++ scripts/mkrpms.sh
@@ -4,7 +4,8 @@ set -x
 # PACKAGES="moxielogic-qemu moxielogic-moxie-elf-binutils bootstrap-moxie-elf-gcc moxielogic-moxie-elf-newlib moxielogic-moxie-elf-gcc moxielogic-moxie-elf-gdb"
 PACKAGES="moxielogic-moxie-elf-binutils moxielogic-moxie-elf-gdb"
 
-for i in moxie-elf moxie-rtems moxiebox; do
+# for i in moxie-elf moxie-rtems moxiebox; do
+for i in moxie-elf; do
     #  PACKAGES="$PACKAGES moxielogic-$i-binutils moxielogic-$i-newlib moxielogic-$i-gcc moxielogic-$i-gdb";
   PACKAGES="$PACKAGES moxielogic-$i-binutils moxielogic-$i-gdb";    
 done;
