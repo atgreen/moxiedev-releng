9818fa9cff74f267356ad080929ddba4d17e29f7
Anthony Green <green@moxielogic.com>
2016-08-30 22:04:56 -0400

    Build bootstrap gcc

--- scripts/mkrpms.sh
+++ scripts/mkrpms.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 set -x
 
+#TARGETS=moxie-elf moxie-rtems moxiebox
+TARGETS=moxie-elf
+
 # PACKAGES="moxielogic-qemu moxielogic-moxie-elf-binutils bootstrap-moxie-elf-gcc moxielogic-moxie-elf-newlib moxielogic-moxie-elf-gcc moxielogic-moxie-elf-gdb"
-PACKAGES="moxielogic-moxie-elf-binutils moxielogic-moxie-elf-gdb"
 
-# for i in moxie-elf moxie-rtems moxiebox; do
-for i in moxie-elf; do
-    #  PACKAGES="$PACKAGES moxielogic-$i-binutils moxielogic-$i-newlib moxielogic-$i-gcc moxielogic-$i-gdb";
-  PACKAGES="$PACKAGES moxielogic-$i-binutils moxielogic-$i-gdb";    
-done;
-
-PACKAGES="moxielogic-repo-$TAG $PACKAGES"
-
-for i in $PACKAGES; do
-  # scrub the yum cache because we probably just placed new content in $REPO 
-  rpmbuild --rebuild dist/$i-*src.rpm;
+for i in $TARGETS; do
+    rpmbuild --rebuild dist/moxielogic-$i-binutils*src.rpm;
+    rpmbuild --rebuild dist/moxielogic-$i-gdb*src.rpm;
+    rpm -hiv /root/rpmbuild/RPMS/x86_64/moxielogic-$i-binutils\*.rpm;
 done
+
+for i in $TARGETS; do
+    rpmbuild --rebuild dist/moxielogic-$i-bootstrap-gcc*src.rpm;
+    rpm -hiv /root/rpmbuild/RPMS/x86_64/moxielogic-$i-bootstrap-gcc\*.rpm;
+done
+
