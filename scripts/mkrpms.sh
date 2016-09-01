3c057c9002e73b455f2032f4c8d93795f397ba5d
Anthony Green <green@moxielogic.com>
2016-09-01 14:35:02 -0400

    add debug output

--- scripts/mkrpms.sh
+++ scripts/mkrpms.sh
@@ -14,15 +14,23 @@ done
 export PATH=/opt/moxielogic/bin:$PATH
 
 for i in $TARGETS; do
+    echo "***** BUILDING $i BOOTSTRAP GCC SOURCE";
     rpmbuild --rebuild dist/bootstrap-$i-gcc*src.rpm;
+    echo "***** INSTALLING $i BOOTSTRAP GCC";
     rpm -hiv /root/rpmbuild/RPMS/x86_64/bootstrap-$i-gcc*.rpm;
+    echo "***** BUILDING $i NEWLIB";
     rpmbuild --rebuild dist/moxielogic-$i-newlib*src.rpm;
+    echo "***** INSTALLING $i NEWLIB";
     rpm -hiv /root/rpmbuild/RPMS/noarch/moxielogic-$i-newlib*.rpm;
+    echo "***** BUILDING $i GCC";
     rpmbuild --rebuild dist/moxielogic-$i-gcc*src.rpm;
+    echo "***** UNINSTALLING $i BOOTSTRAP GCC, BINUTILS, NEWLIB"
     rpm -e bootstrap-$i-gcc
     rpm -e moxielogic-$i-binutils
     rpm -e moxielogic-$i-newlib
 done
+
+echo "***** COPYING ARTIFACTS TO RPMS DIRECTORY"
 
 mkdir rpms
 find /root/rpmbuild -name \*.rpm | xargs -n 1 -I RPMFILE cp -a RPMFILE rpms
