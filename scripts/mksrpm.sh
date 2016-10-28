dc95f82ec38d9d65468a1e4f53723063951e3031
Anthony Green <green@moxielogic.com>
2016-10-27 20:06:21 -0400

    New staged strategy

--- scripts/mksrpm.sh
+++ scripts/mksrpm.sh
@@ -1,4 +1,3 @@
-
 #!/bin/sh
 set -x
 
@@ -50,102 +49,78 @@ for i in moxie-elf moxie-rtems moxiebox; do
   sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-$i-gdb.spec
 done;
 
-#cp specs/moxielogic-rtems4.11.spec.in dist/moxielogic-rtems4.11.spec
-#sed -i "s/@VERSION@/4.11/g" dist/moxielogic-rtems4.11.spec
-#sed -i "s/@DATE@/$DATE/g" dist/moxielogic-rtems4.11.spec
-#sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-rtems4.11.spec
-
-#cp specs/moxielogic-qemu.spec.in dist/moxielogic-qemu.spec
-#sed -i "s/@VERSION@/$QEMU_VERSION/g" dist/moxielogic-qemu.spec
-#sed -i "s/@DATE@/$DATE/g" dist/moxielogic-qemu.spec
-#sed -i "s/@BUILDNUM@/$BUILDNUM/g" dist/moxielogic-qemu.spec
-
-
-#(cd qemu; make clean > /dev/null 2>&1)
-#tar --exclude .git -czf dist/moxielogic-qemu.tar.gz qemu-moxie
-
-#rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" --define "VERSION $QEMU_VERSION" --define "_sourcedir dist" --define "_srcrpmdir dist" -bs dist/moxielogic-qemu.spec
-
 tar \
---exclude .git \
---exclude binutils-gdb/gas \
---exclude binutils-gdb/binutils \
---exclude binutils-gdb/ld \
---exclude binutils-gdb/gold \
---exclude binutils-gdb/gprof \
--czf dist/moxie-gdb.tar.gz binutils-gdb
+  --exclude .git \
+  --exclude binutils-gdb/gas \
+  --exclude binutils-gdb/binutils \
+  --exclude binutils-gdb/ld \
+  --exclude binutils-gdb/gold \
+  --exclude binutils-gdb/gprof \
+  -czf dist/moxie-gdb.tar.gz binutils-gdb
 
 (cd gcc; ./contrib/gcc_update --touch)
-tar \
- --exclude .svn \
- --exclude libjava \
- --exclude libada \
- --exclude gnattools \
- --exclude zlib \
- --exclude libffi \
- --exclude libmudflap \
- --exclude fortran \
- --exclude boehm-gc \
- --exclude libgfortran \
- --exclude ada \
- --exclude libobjc \
- --exclude ppl \
- --exclude cloog-ppl \
- -czf dist/moxie-gcc.tar.gz gcc
 
 tar \
---exclude .git \
---exclude binutils-gdb/gdb \
---exclude binutils-gdb/sim \
---exclude binutils-gdb/readline \
---exclude binutils-gdb/libdecnumber \
---exclude binutils-gdb/gold \
--czf dist/moxie-binutils.tar.gz binutils-gdb
+  --exclude .svn \
+  --exclude libjava \
+  --exclude libada \
+  --exclude gnattools \
+  --exclude zlib \
+  --exclude libffi \
+  --exclude libmudflap \
+  --exclude fortran \
+  --exclude boehm-gc \
+  --exclude libgfortran \
+  --exclude ada \
+  --exclude libobjc \
+  --exclude ppl \
+  --exclude cloog-ppl \
+  -czf dist/moxie-gcc.tar.gz gcc
 
 tar \
---exclude .git \
--czf dist/moxie-newlib.tar.gz newlib
+  --exclude .git \
+  --exclude binutils-gdb/gdb \
+  --exclude binutils-gdb/sim \
+  --exclude binutils-gdb/readline \
+  --exclude binutils-gdb/libdecnumber \
+  --exclude binutils-gdb/gold \
+  -czf dist/moxie-binutils.tar.gz binutils-gdb
+  
+tar \
+  --exclude .git \
+  -czf dist/moxie-newlib.tar.gz newlib
 
 for i in moxie-elf moxie-rtems moxiebox; do
 
-rpmbuild \
---define "_sourcedir dist" \
---define "_srcrpmdir dist" \
---define "VERSION $GDB_VERSION" \
--bs dist/moxielogic-$i-gdb.spec
+  rpmbuild \
+    --define "_sourcedir dist" \
+    --define "_srcrpmdir dist" \
+    --define "VERSION $GDB_VERSION" \
+    -bs dist/moxielogic-$i-gdb.spec
 
-rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" --define "VERSION $GCC_VERSION" --define "_sourcedir dist" --define "_srcrpmdir dist" -bs dist/moxielogic-$i-gcc.spec
+  rpmbuild --nodeps --define "_source_filedigest_algorithm 0" \
+	   --define "_binary_filedigest_algorithm 0" \
+	   --define "VERSION $GCC_VERSION" \
+	   --define "_sourcedir dist" \
+	   --define "_srcrpmdir dist" \
+	   -bs dist/moxielogic-$i-gcc.spec
 
-rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" --define "VERSION $GCC_VERSION" --define "_sourcedir dist" --define "_srcrpmdir dist" -bs dist/bootstrap-$i-gcc.spec
+  rpmbuild --nodeps --define "_source_filedigest_algorithm 0" \
+	   --define "_binary_filedigest_algorithm 0" \
+	   --define "VERSION $GCC_VERSION" \
+	   --define "_sourcedir dist" \
+	   --define "_srcrpmdir dist" \
+	   -bs dist/bootstrap-$i-gcc.spec
 
-rpmbuild \
---define "_sourcedir dist" \
---define "_srcrpmdir dist" \
---define "VERSION $BINUTILS_VERSION" \
--bs dist/moxielogic-$i-binutils.spec
+  rpmbuild --define "_sourcedir dist" \
+	   --define "_srcrpmdir dist" \
+	   --define "VERSION $BINUTILS_VERSION" \
+	   -bs dist/moxielogic-$i-binutils.spec
 
-rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" \
---define "_sourcedir dist" \
---define "_srcrpmdir dist" \
--bs dist/moxielogic-$i-newlib.spec
-
+  rpmbuild --nodeps --define "_source_filedigest_algorithm 0" \
+	   --define "_binary_filedigest_algorithm 0" \
+	   --define "_sourcedir dist" \
+	   --define "_srcrpmdir dist" \
+	   -bs dist/moxielogic-$i-newlib.spec
 done;
 
-#tar \
-#--exclude .git \
-#-czf dist/rtems.tar.gz RTEMS
-
-#rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" \
-#--define "_sourcedir dist" \
-#--define "_srcrpmdir dist" \
-#-bs dist/moxielogic-rtems4.11.spec
-
-cp scripts/RPM-GPG-KEY-MoxieLogic dist
-for TAG in el7; do
-    EXPR=s/TAG/$TAG/g
-    cp scripts/Moxie_Logic.repo.in dist/Moxie_Logic.repo
-    cp specs/moxielogic-repo.spec.in dist/moxielogic-repo-$TAG.spec
-    perl -p -i -e "$EXPR" dist/Moxie_Logic.repo
-    perl -p -i -e "$EXPR" dist/moxielogic-repo-$TAG.spec
-    rpmbuild --nodeps --define "_source_filedigest_algorithm 0" --define "_binary_filedigest_algorithm 0" --define "_sourcedir dist" --define "_srcrpmdir dist" -bs dist/moxielogic-repo-$TAG.spec
-done
